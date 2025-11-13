0280e0e8a2b736ba55371d3134cfe16f816a5067
Andrey Somov <public.somov@gmail.com>
2025-11-13 16:15:37 +0400

    Add module-info.java

---
+++ src/main/java/module-info.java
@@ -0,0 +1,24 @@
+/**
+ * SnakeYAML Engine - A YAML 1.2 processor for Java.
+ */
+module org.snakeyaml.engine {
+    // Main API packages
+    exports org.snakeyaml.engine.v2.api;
+    exports org.snakeyaml.engine.v2.common;
+    exports org.snakeyaml.engine.v2.comments;
+    exports org.snakeyaml.engine.v2.composer;
+    exports org.snakeyaml.engine.v2.constructor;
+    exports org.snakeyaml.engine.v2.emitter;
+    exports org.snakeyaml.engine.v2.env;
+    exports org.snakeyaml.engine.v2.events;
+    exports org.snakeyaml.engine.v2.exceptions;
+    exports org.snakeyaml.engine.v2.nodes;
+    exports org.snakeyaml.engine.v2.parser;
+    exports org.snakeyaml.engine.v2.representer;
+    exports org.snakeyaml.engine.v2.resolver;
+    exports org.snakeyaml.engine.v2.scanner;
+    exports org.snakeyaml.engine.v2.schema;
+    exports org.snakeyaml.engine.v2.serializer;
+    exports org.snakeyaml.engine.v2.tokens;
+    exports org.snakeyaml.engine.v2.util;
+}
