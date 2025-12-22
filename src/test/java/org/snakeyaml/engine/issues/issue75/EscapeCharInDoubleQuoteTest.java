06b1c92821ae6a7acbd7e549e01cb248d962446b
Andrey Somov <public.somov@gmail.com>
2025-12-22 20:32:47 +0400

    Add a test for issue 75

---
+++ src/test/java/org/snakeyaml/engine/issues/issue75/EscapeCharInDoubleQuoteTest.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2018, SnakeYAML
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.snakeyaml.engine.issues.issue75;
+
+import org.junit.jupiter.api.Test;
+import org.snakeyaml.engine.v2.api.Load;
+import org.snakeyaml.engine.v2.api.LoadSettings;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+/**
+ * Test for <a href=
+ * "https://bitbucket.org/snakeyaml/snakeyaml-engine/issues/75/fails-to-parse-valid-json-0x7f-in-quoted">Issue75</a>
+ */
+public class EscapeCharInDoubleQuoteTest {
+  @Test
+  public void testSpecialCharInDoubleQuote() {
+    LoadSettings options = LoadSettings.builder().build();
+    Load load = new Load(options);
+    String str = "\"\u007F\""; // "\DEL"
+    try {
+      String parsed = (String) load.loadFromString(str);
+      fail();
+      assertEquals("", parsed);
+    } catch (Exception e) {
+      assertEquals("special characters are not allowed", e.getMessage());
+    }
+  }
+}
